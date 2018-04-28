ae595e264e2e567462e6176c064ba2a3255ff998
atThegate25 <atthegatewebapp@gmail.com>
2018-04-28 14:41:46 -0500

    files

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require("./routes")
+const bodyParser = require("body-parser");
 const Router = express.Router()
 const mongoose = require("mongoose");
 
@@ -16,6 +17,12 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
 mongoose.connect(MONGODB_URI);
