1b2ec761b5b2c8b9e6535541a950d6ea0fade72c
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-04-29 14:52:14 -0500

    splashpage

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import "./Login.css";
+
+
+class Login extends Component {
+    state = {
+        username: "",
+        password: ""
+    };
+
+handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    if (name === "password") {
+      value = value.substring(0, 15);
+    }
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };  
+
+handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    if (this.state.password.length < 6) {
+      alert(
+        "Choose a more secure password"
+      );
+    } 
+
+    this.setState({
+      username: "",
+      password: ""
+    });
+  };  
+
+render() {
+    return (
+        <div>
+            <form className="form">
+          <input
+            value={this.state.username}
+            name="username"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="username"
+          />
+          <input
+            value={this.state.password}
+            name="password"
+            onChange={this.handleInputChange}
+            type="password"
+            placeholder="Password"
+          />
+          <button onClick={this.handleFormSubmit}>Submit</button>
+        </form>
+
+        </div>    
+    )
+}  
+
+}
+
+export default Login;
---
+++ client/src/components/Splashscreen/SplashScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Login from "../Login";
+import "./SplashScreen.css";
+
+const SplashScreen = () => (
+    <div className="main-container">
+        <img className="background" src="https://lifetomyfullest.files.wordpress.com/2014/04/running-through-airport.jpg" alt="running through airport" height="100%" width="100%" />
+    <div className="panel panel-default">
+       
+        <Login/>   
+      
+      </div>  
+      <div className="panel-heading">
+        <h3 className="panel-title">AT THE GATE</h3>
+      </div>
+      <div className="panel-body">Feeding you before your flight</div>
+
+    </div>
+  
+);
+
+export default SplashScreen;
+
+
+
