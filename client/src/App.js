1bc35a7136a2e7988c3f813b5b02a855378dda17
Khamel <khamel.mumford@gmail.com>
2018-05-01 22:03:11 -0500

    Working w/ App.js (replaced)

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from './components/Login';
+import NavBar from './components/navBar';
+import SplashScreen from './components/Splashscreen';
+import Vendors from './components/Vendors/Vendors'
+import NoMatch from "./components/NoMatch/NoMatch";
+import Flight from "./components/Flight"
+import Cart from "./components/Cart"
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <NavBar />
+            <Switch>
+            {/* <div>
+            <Vendors />
+            </div> */}
+            <Route exact path="/" component={SplashScreen} />
+            {/* <Route component={NoMatch} /> */}
+            <Route exact path="/flight" component={Flight} />
+            <Route exact path="/cart" component={Cart} />
+            </Switch>
+          </div>
+        </Router>
+
+      </div>
+    );
+  }
+}
+
+export default App;
