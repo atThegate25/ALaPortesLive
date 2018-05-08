a34cb37c2c54921728f77bdc23c67d0de1c9171c
Khamel <khamel.mumford@gmail.com>
2018-05-08 14:21:44 -0500

    Styling NavBar v2

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -6,28 +6,33 @@ import * as routes from '../constants/routes';
 import NavInternals from './navBar/NavInternals'
 
 const Navigation = ({ authUser }) =>
-   <div>
-       <NavInternals />
-       {authUser
-           ? <NavigationAuth />
-           : <NavigationNonAuth />
-       }
-   </div>
+    <div>
+        <NavInternals />
+        {authUser
+            ? <NavigationAuth />
+            : <NavigationNonAuth />
+        }
+    </div>
 
 const NavigationAuth = () =>
-   <ul>
-       {/* <li><Link to={routes.LANDING}>Landing</Link></li>
+    <ul>
+        {/* <li><Link to={routes.LANDING}>Landing</Link></li>
        <li><Link to={routes.HOME}>Home</Link></li>
        <li><Link to={routes.ACCOUNT}>Account</Link></li> */}
-       <li><Link to={routes.LANDING}><SignOutButton /></Link></li>
-   </ul>
+        <li><Link to={routes.LANDING}><SignOutButton /></Link></li>
+    </ul>
 
 const NavigationNonAuth = () =>
-   
-<NavInternals />
-//    <ul>
-//        <li><Link to={routes.LANDING}><button className="btn">Landing</button></Link></li>
-//        <li><Link to={routes.SIGN_IN}><button className="btn btn-info">Sign In</button></Link></li>
-//    </ul>
 
+    <NavInternals >
+        <br />
+        <br />
+        <br />
+        <div>
+            <ul>
+                <li><Link to={routes.LANDING}><button className="btn">Landing</button></Link></li>
+                <li><Link to={routes.SIGN_IN}><button className="btn btn-info">Sign In</button></Link></li>
+            </ul>
+        </div>
+    </NavInternals>
 export default Navigation;
