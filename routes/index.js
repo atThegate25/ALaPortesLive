33dd1d14ce3ecde01a76a9fb021ad0a4a9c98283
Khamel <khamel.mumford@gmail.com>
2018-05-08 19:29:09 -0500

    adding cart images + map

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ const orderRoute = require("./apiRoutes");
 router.use("/", orderRoute)
 // router.use("/", htmlRouter)
 
-router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, '../client/public/index.html'));
-});
+// router.use(function(req, res) {
+//     res.sendFile(path.join(__dirname, '../client/public/index.html'));
+// });
 
 module.exports = router;
