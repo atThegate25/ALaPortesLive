00b4749e869c15eb39c6206ab0ef68d2acd98dd5
Khamel <khamel.mumford@gmail.com>
2018-05-11 00:15:59 -0500

    Added 3 Cart components

--- client/src/components/Carts/AddtoCartMCD.js
+++ client/src/components/Carts/AddtoCartMCD.js
@@ -1,106 +1,69 @@
 import React, { Component } from "react";
 import axios from "axios";
-import mcDeeMenu from './CartUtils/mcdonalds.json'
+import '../css'
+import menuItems from './CartUtils/mcdonalds.json'
+import InputComponent1 from './InputComponent1'
+import Cart from './Cart'
 
-class AddtoCartMCD extends Component {
+export default class AddtoCartMCD extends Component {
     state = {
-        name: "hardcode",
-        quantity: 1
+        // name: menuItems[0].name,
+        value: 0,
+        products: []
+
     }
 
-    handleInputChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        console.log(name + value)
-        this.setState({
-            [name]: value
+    updateCart = () => {
+        axios.get("/pbyapi/api/").then((response) => {
+
+            this.setState({
+                products: response.data
+            })
+            console.log(this.state.products)
         });
     }
-    addProductHandler = (event) => { console.log(mcDeeMenu[0].name)
+    deleteItem = (id) => {
+        // const id = event.target.value;
+        axios.delete(`/pbyapi/api/${id}`).then(response => {
+            this.setState({
+                products: this.state.products.filter(product => product._id !== id)
+            });
+        });
+    }
+    addProductHandler = (name, price, quantity, image) => {
+        // console.log(event)
         const data = {
-            name: mcDeeMenu[0].name,
-            quantity: this.state.quantity
+            name: name,
+            quantity: quantity,
+            price: price,
+            image: image
         };
         console.log(data)
-        axios.post("/api/", data).then(data => console.log("Success!"+data));
-    }
 
+        axios.post("/pbyapi/api/", data).then(data => this.updateCart());
+
+    }
     render() {
         return (
-            <div>
-                <input placeholder="Product Name"
-                    name="name"
-                    value={this.state.name}
-                    onChange={this.handleInputChange} />
-                <input placeholder="Product quantity"
-                    type="number"
-                    name="quantity"
-                    value={this.state.quantity}
-                    onChange={this.handleInputChange} />
-                <button onClick={this.addProductHandler}>Add Product</button>
-
-
-                <div className="row">
-                    <div className="row">
-                        <div className="col-md-10 col-md-offset-1 product-section-divider">
-                            <h2 className="text-center">$2 Items</h2>
-                            <div className="row">
-                                <div className="col-md-3 col-xs-6 product-image text-center">
-                                    <img src="https://www.mcdonalds.com/content/dam/usa/documents/d123/products/bacon_mcdouble.png" className="img-responsive center-block" alt="Bacon McDouble®" title="" />
-                                    <h3>Bacon McDouble® $10.00</h3>
-                                    <a href="https://www.mcdonalds.com/us/en-us/product/bacon-mcdouble.html" target="_self" data-at="D123LandingPage:Bacon McDouble" data-track="promotion" className="learn-more" aria-label="Learn More About Bacon McDouble®">Learn more  </a>
-                                    <br />
-                                    <input placeholder="Product quantity"
-                                        type="number"
-                                        name={mcDeeMenu[0].name}
-                                        value={this.state.quantity}
-                                        onChange={this.handleInputChange} />
-                                    <button onClick={this.addProductHandler} className="btn btn-danger my-cart-btn" data-id="1" data-name="product 1" data-summary="summary 1" data-price="10" data-quantity="1" data-image="">  <strong>Add to Cart</strong></button>
-                                </div>
-                                <div className="col-md-3 col-xs-6 product-image text-center">
-                                    <img src="https://www.mcdonalds.com/content/dam/usa/documents/d123/products/tenders.png" className="img-responsive center-block" alt="2 Pc. Buttermilk Crispy Tenders" title="" />
-                                    <h3>2 Pc. Buttermilk Crispy Tenders Are good</h3>
-                                    <a href="https://www.mcdonalds.com/us/en-us/product/buttermilk-crispy-chicken-tenders-4-piece.html" target="_self" data-at="D123LandingPage:2 Pc Buttermilk Crispy Tenders" data-track="promotion" className="learn-more" aria-label="Learn More About 2 Pc. Buttermilk Crispy Tenders">Learn more  </a>
-                                    <br />
-                                    <input placeholder="Product quantity"
-                                        type="number"
-                                        name="quantity"
-                                        value={this.state.quantity}
-                                        onChange={this.handleInputChange} />
-                                    <button onClick={this.addProductHandler} className="btn btn-danger my-cart-btn" data-id="1" data-name="product 1" data-summary="summary 1" data-price="10" data-quantity="1" data-image="">  <strong>Add to Cart</strong></button>
-                                </div>
-                                <div className="col-md-3 col-xs-6 product-image text-center">
-                                    <img src="https://www.mcdonalds.com/content/dam/usa/documents/d123/products/mcgriddle.png" className="img-responsive center-block" alt="Sausage McGriddles®" title="" />
-                                    <h3>Sausage McGriddles®</h3>
-                                    <a href="https://www.mcdonalds.com/us/en-us/product/sausage-mcgriddles.html" target="_self" data-at="D123LandingPage:Sausage McGriddles" data-track="promotion" className="learn-more" aria-label="Learn More About Sausage McGriddles®">  Learn more  </a>
-                                    <br />
-                                    <input placeholder="Product quantity"
-                                        type="number"
-                                        name="quantity"
-                                        value={this.state.quantity}
-                                        onChange={this.handleInputChange} />
-                                    <button onClick={this.addProductHandler} className="btn btn-danger my-cart-btn" data-id="1" data-name="product 1" data-summary="summary 1" data-price="10" data-quantity="1" data-image="">  <strong>Add to Cart</strong></button>
-                                </div>
-                                <div className="col-md-3 col-xs-6 product-image text-center">
-                                    <img src="https://www.mcdonalds.com/content/dam/usa/documents/d123/products/mccafe.png" className="img-responsive center-block" alt="Hot &amp; cold McCafé® beverages in glass cups" title="" />
-                                    <h3>Small McCafé®</h3>
-                                    <br />
-                                    <a href="https://www.mcdonalds.com/us/en-us/full-menu/mccafe.html" target="_self" data-at="D123LandingPage:Small McCafé" data-track="promotion" className="learn-more" aria-label="Learn More About Small McCafé®">Learn more </a>
-                                    <br />
-                                    <input placeholder="Product quantity"
-                                        type="number"
-                                        name="quantity"
-                                        value={this.state.quantity}
-                                        onChange={this.handleInputChange} />
-                                    <button onClick={this.addProductHandler} className="btn btn-danger my-cart-btn" data-id="1" data-name="product 1" data-summary="summary 1" data-price="10" data-quantity="1" data-image="">  <strong>Add to Cart</strong></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <body className="page-body">
+            <section className="page-wrapper innerpage-section-padding">
+            <div id="menu-page" className="menu-list">
+            <div className="container-fluid text-center menu">
+                <div id="lunch-dishes" className="tab-pane fade active in">
+                    {menuItems.map(menuItem => {
+                        return (
+                            <InputComponent1 menuItem={menuItem}
+                                addToCart={this.addProductHandler} />
+                        )
+                    })}
                 </div>
-                {this.state.quantity > 0 && <dummycomponent />}
+                <Cart update={this.updateCart}
+                    products={this.state.products}
+                    delete={this.deleteItem} />
+            </div >
             </div>
+            </section>
+            </body>
         );
     }
 }
-export default AddtoCartMCD;
